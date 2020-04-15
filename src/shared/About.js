3a1b4db79a22aca9022c11b74440138c89f158ad
Shane Barnwell <ksbarnwell@gmail.com>
2020-04-15 13:15:39 -0600

    new tab

--- src/shared/About.js
+++ src/shared/About.js
@@ -7,12 +7,12 @@ class About extends Component {
         return (
             <div className="content">
                 <p>
-                    I am a Senior Programmer at <a href="https://macu.com">Mountain America Credit Union</a>, currently focusing on building their
+                    I am a Senior Programmer at <a target="_blank" rel="noopener noreferrer" href="https://macu.com">Mountain America Credit Union</a>, currently focusing on building their
                     member and loan origination application. I'm developing in TypeScript, React, Redux, Sass, C#, .NET and SQL with other highly skilled
                     developers.
                 </p>
                 <p>
-                    Before working at MACU I worked for <a href="https://www.overstock.com" target="_blank">Overstock.com</a> for 7 years in several capacities, including
+                    Before working at MACU I worked for <a target="_blank" rel="noopener noreferrer" href="https://www.overstock.com" target="_blank">Overstock.com</a> for 7 years in several capacities, including
                     Team Lead and Senior Frontend Developer. I was primarily responsible for the customer facing Cart and Checkout pages of the shopping website.
                     During my employment I learned a lot about application architecture, analytics, and customer conversion. I also learned how to use some amazing technologies such as Node.js,
                     React, Redux, Express, Splunk, CI/CD Pipelines, Docker, and many others.
